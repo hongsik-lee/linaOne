1190183548b24baa0f097d804e2181106a4da048
LAPTOP-AR0T5J6U\NETIVERED <hslee1166@gmail.com>
2022-11-21 17:53:01 +0900

    OVERVIEW 숫자카운팅 효과 추가

--- assets/js/common.js
+++ assets/js/common.js
@@ -47,3 +47,37 @@ document.addEventListener("scroll", () => {
 });
 
 goTop.addEventListener("click", goToTop);
+
+// OVERVIEW 숫자 카운팅
+var counted = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (counted == 0 && $(window).scrollTop() > oTop) {
+    $('.count-num').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    counted = 1;
+  }
+
+});
