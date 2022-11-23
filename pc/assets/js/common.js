49b2368d416900cf61296ba8bebb8dc01c049770
LAPTOP-AR0T5J6U\NETIVERED <hslee1166@gmail.com>
2022-11-23 15:00:47 +0900

    no message

--- pc/assets/js/common.js
+++ pc/assets/js/common.js
@@ -145,12 +145,15 @@ childTabs.forEach(tab => {
     });
 });
 
-function openPopup(width, height, link) {
-    const popupWidth = width
-        , popupHeight = height
-        , popupX = (window.screen.width / 2) - (popupWidth / 2)
-        , popupY = (window.screen.height / 2) - (popupHeight / 2)
-        , options = 'width='+popupWidth+', height='+popupHeight+', top='+popupY+', left='+popupX+', resizable=yes, scrollbars=yes';
-    
-    window.open('./pages/foot/'+ link +'.html', '_blank', options);
+function openPop(url, title, w, h) {
+    var screenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
+    var screenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+
+    width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+    height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+    var left = ((width / 2) - (w / 2)) + screenLeft;
+    var top = ((height / 2) - (h / 2)) + screenTop;
+
+    window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
 }
