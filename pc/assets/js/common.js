2d06916c472cab95b5ba628983e6e4276119d815
cheonsu92 <cheonsu92@gmail.com>
2022-11-28 10:44:27 +0900

    메인 수정

--- pc/assets/js/common.js
+++ pc/assets/js/common.js
@@ -1,87 +1,36 @@
-
-// 컨텐츠 scroll 공통 효과
-$(function(){
-    AOS.init({
-        easing: 'ease-in-out-sine',
-        duration:500
-      });
-
-    onElementHeightChange(document.body, function(){
-        AOS.refresh();
-      });
-})
-
-function onElementHeightChange(elm, callback) {
-    var lastHeight = elm.clientHeight
-    var newHeight;
-    
-    (function run() {
-        newHeight = elm.clientHeight;
-        if (lastHeight !== newHeight) callback();
-        lastHeight = newHeight;
-
-        if (elm.onElementHeightChangeTimer) {
-          clearTimeout(elm.onElementHeightChangeTimer); 
-        }
-
-        elm.onElementHeightChangeTimer = setTimeout(run, 200);
-    })();
-}
-
-// 아코디언 공통 스크립트
-const accor_tit = document.querySelectorAll(".acc_tit_area");
-accor_tit.forEach((accorTit) => {
-    accorTit.addEventListener("click", () => {
-        if (accorTit.classList.contains("acc-active")) {
-            accorTit.classList.remove("acc-active");
-        } else {
-            const accor_active = document.querySelectorAll(".acc-active");
-            accor_active.forEach((active) => {
-                active.classList.remove("acc-active");
-            });
-            accorTit.classList.add("acc-active");
-        }
-    });
-});
-
 window.addEventListener("DOMContentLoaded", function () {
     subPageVisualAnimation();
 
-    // Tab Content 
-    const parentTabs = document.querySelectorAll('[data-tab-target]');
-    const tabContents = document.querySelectorAll('[data-tab-content]');
-    const childTabs = document.querySelectorAll('[data-sub-target]');
-    const subContents = document.querySelectorAll('[data-sub-content]');
-
-    $('[data-tab-target]').each((index , tab) => {
-        $(tab).on('click', (e) => {
-            e.preventDefault();
-
-            let target = document.querySelector(tab.dataset.tabTarget);
-            tabContents.forEach(tabContent => {
-                tabContent.classList.remove('active')
-            });
-            parentTabs.forEach(tab => {
-                tab.classList.remove('active')
-            });
-            target.classList.add('active')
-            tab.classList.add('active')
-        })
+    // 컨텐츠 scroll 공통 효과
+    AOS.init({
+        easing: 'ease-in-out-sine',
+        duration:500
     });
 
-    $('[data-sub-target]').each((index , tab) => {
-        $(tab).on('click', (e) => {
-            e.preventDefault();
+    onElementHeightChange(document.body, function(){
+        AOS.refresh();
+    });
 
-            let target = document.querySelector(tab.dataset.subTarget);
-            subContents.forEach(tabContent => {
-                tabContent.classList.remove('active')
-            });
-            childTabs.forEach(tab => {
-                tab.classList.remove('active')
-            });
-            target.classList.add('active')
-            tab.classList.add('active')
+    // tab
+    const $parentTabs = $('[data-tab-target]');
+    $parentTabs.each(function(index, item) {
+        $(item).on('click' , function(e) {
+            handleParentTabClick(e);
+        });
+    });
+
+    const $childTabs = $('[data-sub-target]');
+    $childTabs.each(function(index, item) {
+        $(item).on('click' , function(e) {
+            handleChildTabClick(e);
+        });
+    });
+
+    // accordion
+    const $accTitArea = $('.acc_tit_area');
+    $accTitArea.each(function(index, item) {
+        $(item).on('click', function(e) {
+            handleAccTitAreaClick(e);
         })
     });
 
@@ -93,34 +42,8 @@ window.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-// 서브페이지 비쥬얼 글자 에니메이션
-const subPageVisualAnimation = () => {
-    const $subWarp = $('.sub_wrap')
-        , $titleAnim = $('.page-tit-line')
-        , $visualBgAnim = $('.visual-sec');
-
-    if($subWarp.length > 0) {
-        $titleAnim.each((index, item) => {
-            const $target = $(item);
-    
-            if(!$target.hasClass('active')) {
-                $target.addClass('active')
-            }
-        })
-    
-        $visualBgAnim.each((index, item) => {
-            const $target = $(item);
-    
-            if(!$target.hasClass('active')) {
-                $target.addClass('active')
-            }
-        });
-    }
-}
-
 $(window).scroll(function () {
     const $counter = $("#counter");
-
     if($counter.length > 0) {
         const oTop = $("#counter").offset().top - window.innerHeight;
 
@@ -136,8 +59,100 @@ $(window).scroll(function () {
     }
 });
 
+const onElementHeightChange = function(elm, callback) {
+    let lastHeight = elm.clientHeight
+    let newHeight;
+    
+    (function run() {
+        newHeight = elm.clientHeight;
+        if (lastHeight !== newHeight) callback();
+        lastHeight = newHeight;
+
+        if (elm.onElementHeightChangeTimer) {
+          clearTimeout(elm.onElementHeightChangeTimer); 
+        }
+
+        elm.onElementHeightChangeTimer = setTimeout(run, 200);
+    })();
+}
+
+// 아코디언 공통 스크립트
+const handleAccTitAreaClick = function(item) {
+    const $target = $(item)
+        , $accorActive = $('.acc-active');
+    
+    if($target.hasClass('acc-active')) {
+        $target.removeClass('acc-active')
+    } else {
+        $accorActive.each(function(index, item) {
+            $(item).removeClass('acc-active');
+        });
+        $target.addClass('acc-active')
+    }
+}
+
+// 탭 공통 스크립트
+const handleParentTabClick = function(e) {
+    e.preventDefault();
+    const $target = $(e.target)
+        , dataTabTarget = $target.data('tab-target')
+        , $tab = $(dataTabTarget)
+        , $tabContents = $('[data-tab-content]');
+
+    for(let i = 0; i < $tabContents.length; i++) {
+        let $tabContent = $($tabContents[i]);
+        $tabContent.removeClass('active');
+    }
+
+    $target.closest('li').siblings().children('a').removeClass('active');
+    $target.addClass('active');
+    $tab.addClass('active');
+}
+
+const handleChildTabClick = function(e) {
+    e.preventDefault();
+    const $target = $(e.target)
+        , dataSubTabTarget = $target.data('sub-target')
+        , $subTab = $(dataSubTabTarget)
+        , $subTabContents = $('[data-sub-content]');
+
+    for(let i = 0; i < $subTabContents.length; i++) {
+        let $subTabContent = $($subTabContents[i]);
+        $subTabContent.removeClass('active');
+    }
+
+    $target.closest('li').siblings().children('a').removeClass('active');
+    $target.addClass('active');
+    $subTab.addClass('active');
+}
+
+// 서브페이지 비쥬얼 에니메이션
+const subPageVisualAnimation = function() {
+    const $subWarp = $('.sub_wrap')
+        , $titleAnim = $('.page-tit-line')
+        , $visualBgAnim = $('.visual-sec');
+
+    if($subWarp.length > 0) {
+        $titleAnim.each((index, item) => {
+            const $target = $(item);
+    
+            if(!$target.hasClass('active')) {
+                $target.addClass('active')
+            }
+        });
+    
+        $visualBgAnim.each((index, item) => {
+            const $target = $(item);
+    
+            if(!$target.hasClass('active')) {
+                $target.addClass('active')
+            }
+        });
+    }
+}
+
 // OVERVIEW 숫자 카운팅 에니메이션
-const increaseNumberAnimation = (elem, duration) => {
+const increaseNumberAnimation = function(elem, duration) {
     let startTimeStamp;
     const $target = $(elem)
         , start = $target.data('start-count')
@@ -170,12 +185,12 @@ const increaseNumberAnimation = (elem, duration) => {
     requestAnimationFrame(step);
 };
 
-const setNumberComma = (num) => {
+const setNumberComma = function(num) {
     return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-// popup
-const openPopup = (url, w, h) => {
+// window popup
+const openPopup = function(url, w, h) {
     var screenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
     var screenTop = window.screenTop != undefined ? window.screenTop : screen.top;
 
