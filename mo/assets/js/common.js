b6572037eca1abd5b1646cdc37bf05a3b24ef5e1
hyesu <hslee1166@gmail.com>
2022-12-05 13:49:26 +0900

    header nav

---
+++ mo/assets/js/common.js
@@ -0,0 +1,8 @@
+
+// header nav animation
+function openNav(){
+    document.getElementById('gnb-wrap').classList.add('on')
+}
+function closeNav(){
+    document.getElementById('gnb-wrap').classList.remove('on')
+}
